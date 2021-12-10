b01644f202c54897c202d4c248aa899b3ff808dc
rejo-philp <rejophilipjose@gmail.com>
2021-12-10 12:13:30 +0530

    add fii_equity

---
+++ test_fii_equty.py
@@ -0,0 +1,26 @@
+from nse_arc_lib import fii_latest_values as fii
+import os 
+import pandas as pd
+
+
+# df_obj = fii()
+# print(df_obj)
+
+
+if __name__ == '__main__' :
+
+    current_fii_dii_value = fii()
+    if(type(current_fii_dii_value) == None):
+        print("status not retrived")
+        exit()
+    # import pdb;pdb.set_trace()
+    # import pdb;pdb.set_trace()
+    path_status = os.path.exists("./fii_dii.csv")
+    if(path_status == True  ):
+        Source_df = pd.read_csv("./fii_dii.csv")
+        current_fii_dii_value = current_fii_dii_value.append(Source_df, ignore_index=True)
+
+
+    current_fii_dii_value.to_csv('./fii_dii.csv')
+
+    
