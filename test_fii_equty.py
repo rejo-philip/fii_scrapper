96f6baa6ef40f64613acd15d995870a5c9ba80f9
rejo-philp <rejophilipjose@gmail.com>
2022-01-05 16:39:15 +0530

    adding files

--- test_fii_equty.py
+++ test_fii_equty.py
@@ -15,9 +15,11 @@ if __name__ == '__main__' :
         exit()
     # import pdb;pdb.set_trace()
     # import pdb;pdb.set_trace()
+    
     path_status = os.path.exists("./fii_dii.csv")
     if(path_status == True  ):
         Source_df = pd.read_csv("./fii_dii.csv")
+        Source_df  = Source_df[['category', 'date', 'buyValue', 'sellValue', 'netValue']]
         current_fii_dii_value = current_fii_dii_value.append(Source_df, ignore_index=True)
 
 
